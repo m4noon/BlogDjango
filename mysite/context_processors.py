3e9d96bbd6a0b581e8a7eb48dfdbe2bf5844f56c
m4noon <thejonka@gmail.com>
2022-06-30 11:31:49 +0300

    rm widget popular news

--- mysite/context_processors.py
+++ mysite/context_processors.py
@@ -1,16 +1,10 @@
 from category.models import Category
-from blog.models import Post
 from tag.models import Tag
 
 
 def categories(request):
     categories = Category.objects.all
     return {'categories': categories}
-
-
-def posts(request):
-    posts = Post.objects.all()[:5]
-    return {'posts': posts}
 
 
 def tags(request):
